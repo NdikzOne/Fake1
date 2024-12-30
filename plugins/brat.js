33000db4e78b5bfc1364e73479dbf002c0fbc01b
ndikzone <baksocloud@gmail.com>
2024-12-30 23:32:55 +0700

    Update brat.js

--- plugins/brat.js
+++ plugins/brat.js
@@ -1,4 +1,6 @@
 let fetch = require("node-fetch")
+const { addExif } = require('../lib/sticker.js')
+const { Sticker } = require('wa-sticker-formatter')
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, usedPrefix, command, text }) => {
@@ -8,7 +10,8 @@ m.reply(wait)
   let vas = await res.json()
   let v = vas.data
     
-    conn.sendFile(m.chat, v.url, null, wm, m)
+    let stiker = await createSticker(null, v.url, packname, author)
+  conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
 }
 handler.help = ['brat']
 handler.tags = ['maker']
@@ -16,3 +19,16 @@ handler.command = /^(brat)$/i
 handler.limit = true
 
 module.exports = handler
+
+async function createSticker(img, url, packName, authorName, quality) {
+	let stickerMetadata = {
+		type: 'full',
+		pack: global.packname,
+		author: global.author,
+		quality
+	}
+	return (new Sticker(img ? img : url, stickerMetadata)).toBuffer()
+}
+function pickRandom(list) {
+    return list[Math.floor(list.length * Math.random())];
+}
