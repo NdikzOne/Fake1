0f5dcab21ddb613f5340ac298c80c24e87b8a777
ndikzone <baksocloud@gmail.com>
2025-01-06 10:02:00 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -2,23 +2,14 @@ let fetch = require('node-fetch')
 let fs = require('fs')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args || !args[0]) throw `✳️ Contoh :\n${usedPrefix + command} https://www.tiktok.com/xxxxx`
-    if (!(args[0].includes('http://') || args[0].includes('https://'))) return m.reply(`url invalid, please input a valid url. Try with add http:// or https://`)
+    if (!(args[0].includes('https://') || args[0].includes('https://'))) return m.reply(`url invalid, please input a valid url. Try with add http:// or https://`)
     if (!args[0].includes('tiktok.com')) return m.reply(`Invalid Tiktok URL.`)
  //   if (!args[0]) throw `*• Example:* ${usedPrefix + command} https://youtu.be/xxxx`
     m.reply(wait)
-let nt = await fetch(`https://api.tiklydown.eu.org/api/download?url=${args[0]}`)
+let nt = await fetch(`${neNdikz}api/tiktok?url=${args[0]}&apikey=${neoapi}`)
         let vas = await nt.json()
-  let v = vas.video
-  let s = vas.author
-        let capt = `*「 T I K T O K 」*
-
-*📛Author:* ${s.name}
-*📒Title:* ${vas.title}
-
-KETIK .ttmp3 UNTUK MENDOWNLOAD AUDIO
-${wm}`
-        console.log(vas.url[0])
-        conn.sendFile(m.chat, v.noWatermark, null, capt, m)
+  let v = vas.data
+        conn.sendFile(m.chat, v.video, null, wm, m)
         conn.sendMessage(m.chat, {
             react: {
                 text: "✅",
